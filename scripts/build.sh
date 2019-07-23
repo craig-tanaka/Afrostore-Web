bb08fd32e9f7cba9d92e0e69d83a11cfa9d3866b
Tanxy Rogue <tanxyrogue@gmail.com>
2019-07-23 16:04:55 +0200

    fixed build task

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 echo "_____________________________ Beginning Pre-Deploy Build _____________________________"
 mkdir public 
-cp -a $TRAVIS_BUILD_DIR/src/ $TRAVIS_BUILD_DIR/public/
+cp -r $TRAVIS_BUILD_DIR/src/. $TRAVIS_BUILD_DIR/public/
 cd public && ls
 echo "_____________________________ Beginning Deploy _____________________________"
