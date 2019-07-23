01c70113bd29dced204311f549b7cb3f73f1488c
Tanxy Rogue <tanxyrogue@gmail.com>
2019-07-23 15:55:22 +0200

    display public folder contents before deploying

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 echo "_____________________________ Beginning Pre-Deploy Build _____________________________"
 mkdir public 
 cp -a $TRAVIS_BUILD_DIR/src/ $TRAVIS_BUILD_DIR/public/
-ls
+cd public && ls
 echo "_____________________________ Beginning Deploy _____________________________"
