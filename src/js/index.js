76fa2b970db2d98519455672fb4e40cacab1db97
Tanxy Rogue <tanxyrogue@gmail.com>
2019-08-08 05:43:30 +0200

    setup project for firestore downloads

--- src/js/index.js
+++ src/js/index.js
@@ -1,37 +1,51 @@
-const nav = document.querySelector('nav')
-const cartBtnPaths = document.querySelectorAll('.cart-btn path')
-const menuBtnPaths = document.querySelectorAll('.menu-btn path')
-const productArr = document.querySelectorAll('.product')
+/*jshint esversion: 6 */
+
+const nav = document.querySelector('nav');
+const cartBtnPaths = document.querySelectorAll('.cart-btn path');
+const menuBtnPaths = document.querySelectorAll('.menu-btn path');
+const productArr = document.querySelectorAll('.product');
+
+const db = firebase.firestore();
 
 window.addEventListener('scroll', (_event)=>{
     if ((document.body.scrollTop || document.documentElement.scrollTop) >= 12){
-        nav.style.backgroundColor = "var(--blacks)"
+        nav.style.backgroundColor = "var(--blacks)";
         cartBtnPaths.forEach((el) => {
-            el.style.fill = "white"
-        })
+            el.style.fill = "white";
+        });
         menuBtnPaths.forEach((el)=>{
-            el.style.stroke = 'white'
-        })
+            el.style.stroke = 'white';
+        });
     }
     else{
-        nav.style.backgroundColor = "white"
+        nav.style.backgroundColor = "white";
         cartBtnPaths.forEach((el) => {
-            el.style.fill = "var(--blacks)"
-        })
+            el.style.fill = "var(--blacks)";
+        });
         menuBtnPaths.forEach((el)=>{
-            el.style.stroke = 'var(--blacks)'
-        })
+            el.style.stroke = 'var(--blacks)';
+        });
     }
-},false)
+},false);
 
 productArr.forEach(el => {
     el.addEventListener('click', (__event)=>{
-        alert('Santoryou has')
-    })
-})
-
-db.collection("users").get().then((querySnapshot) => {
-    querySnapshot.forEach((doc) => {
-        alert(`${doc.id} => ${doc.data()}`);
+        alert('Santoryou has');
     });
 });
+
+
+function setProducts(params) {
+    
+}
+
+function getRecommended() {
+
+    db.collection("products").get()
+    .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            alert(`${doc.id} => ${doc.data().ProductName}`);
+        });
+    });
+    
+}
