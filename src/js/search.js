9647a040c882305b54b5cea9c171981d41e90ef7
Craig Tanaka Mudzingwa <tanxyrogue@gmail.com>
2019-09-21 15:45:50 +0000

    Fixed searched product styling on search page

--- src/js/search.js
+++ src/js/search.js
@@ -13,11 +13,11 @@ db.collection('products').where('Tags', 'array-contains',url.searchParams.get('s
                     <img src="https://firebasestorage.googleapis.com/v0/b/${bucket}/o/product-images%2F${doc.id}%2F00.jpg?alt=media" alt="" class="searched-images">
                     <h6>${data.ProductName}</h6>
                     <div>
-                        <svg class="addwish-btn" viewBox="0 0 24 24" fill="var(--white)" fill-rule="evenodd" clip-rule="evenodd">
+                        <svg class="addwish-btn" viewBox="0 0 24 24" fill="var(--blacks)" fill-rule="evenodd" clip-rule="evenodd">
                             <path d="M12 21.593C6.37 16.054 1 11.296 1 7.191 1 3.4 4.068 2 6.281 2c1.312 0 4.151.501 5.719 4.457 1.59-3.968 4.464-4.447 5.726-4.447C20.266 2.01 23 3.631 23 7.191c0 4.069-5.136 8.625-11 14.402M17.726 1.01c-2.203 0-4.446 1.042-5.726 3.238C10.715 2.042 8.478 1 6.281 1 3.098 1 0 3.187 0 7.191 0 11.852 5.571 16.62 12 23c6.43-6.38 12-11.148 12-15.809 0-4.011-3.095-6.181-6.274-6.181"/>
                         </svg>
                         <p class="Price">$${data.Price}</p>
-                        <svg class="add2cart-btn"  viewBox="0 0 24 24"  stroke="var(--white)"  >
+                        <svg class="add2cart-btn"  viewBox="0 0 24 24"  stroke="var(--blacks)"  >
                             <line x1="5" y1="20" x2="10" y2="20" stroke-width="1"/>
                             
                             <line x1="5" y1="20" x2="3" y2="7" stroke-width="1"/>
