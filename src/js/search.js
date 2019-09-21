c96d9a77e4318e362cc1338fe18c74320757fedd
Craig Tanaka Mudzingwa <tanxyrogue@gmail.com>
2019-09-21 18:51:59 +0000

    Added anonymous user cart functionality on search page

--- src/js/search.js
+++ src/js/search.js
@@ -17,7 +17,7 @@ db.collection('products').where('Tags', 'array-contains',url.searchParams.get('s
                             <path d="M12 21.593C6.37 16.054 1 11.296 1 7.191 1 3.4 4.068 2 6.281 2c1.312 0 4.151.501 5.719 4.457 1.59-3.968 4.464-4.447 5.726-4.447C20.266 2.01 23 3.631 23 7.191c0 4.069-5.136 8.625-11 14.402M17.726 1.01c-2.203 0-4.446 1.042-5.726 3.238C10.715 2.042 8.478 1 6.281 1 3.098 1 0 3.187 0 7.191 0 11.852 5.571 16.62 12 23c6.43-6.38 12-11.148 12-15.809 0-4.011-3.095-6.181-6.274-6.181"/>
                         </svg>
                         <p class="Price">$${data.Price}</p>
-                        <svg class="add2cart-btn"  viewBox="0 0 24 24"  stroke="var(--blacks)"  >
+                        <svg class="add2cart-btn"  viewBox="0 0 24 24"  stroke="var(--blacks)" pid="${doc.id}">
                             <line x1="5" y1="20" x2="10" y2="20" stroke-width="1"/>
                             
                             <line x1="5" y1="20" x2="3" y2="7" stroke-width="1"/>
@@ -45,7 +45,30 @@ db.collection('products').where('Tags', 'array-contains',url.searchParams.get('s
                 window.open(`./Product.html?p=${querySnapshot.docs[index].id}`, '_self')
             })
         })
+
+        document.querySelectorAll('.add2cart-btn').forEach((el, index) => {
+            el.addEventListener('click', event => {
+                event.stopImmediatePropagation();
+                addToCart(event);
+            })
+        })
     })
     .catch(error => {
         console.log("Error getting documents: ", error)
     });
+
+    function addToCart(e) {
+        if(firebase.auth().currentUser === null){
+            if(sessionStorage.getItem('cartItems') === null){
+                sessionStorage.setItem('cartItems', JSON.stringify([]));
+            }
+            let cartProducts = JSON.parse(sessionStorage.getItem('cartItems'));
+            let pid = e.currentTarget.getAttribute('pid');
+            if(!cartProducts.includes(pid)){
+                cartProducts[cartProducts.length] = pid;
+                sessionStorage.setItem('cartItems', JSON.stringify(cartProducts));
+                console.log(JSON.parse(sessionStorage.getItem('cartItems')));
+                // TODO do something responsive to show user item added o cart
+            }
+        }
+    }
