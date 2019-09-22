b98b1b88464a871578d597409ddbfbcb3efbb843
Craig Tanaka Mudzingwa <tanxyrogue@gmail.com>
2019-09-22 20:24:46 +0000

    Fixed sign in cart copying method

--- src/js/app.js
+++ src/js/app.js
@@ -39,6 +39,17 @@ function changeCartIcon() {
         d="m57.41903,136c4.5,0 8.4,-3.1 9.4,-7.4l16.9,-70.1c0.3,-1.1 0.7,-2.2 1.4,-3.2c2.4,-3.7 5.6,-6.8 9.4,-9.1c1.4,-0.8 3.1,-1.2 4.7,-1.2l30.1,0c3.6,0 6.9,2 8.6,5.2c1.1,2 2.8,3.6 4.8,4.7c1.4,0.8 3,1.2 4.6,1.1l70.9,0s3.5,0 7.6,-6.6c1.8,-2.8 4.8,-4.5 8.1,-4.4l29.4,0c1.4,0 2.9,0.3 4.1,0.9c3.3,1.6 8.4,4.8 12.7,14.1c0.5,1.1 2,3.4 2,4.6c0.7,13.9 9.9,49.1 14,64.2c1.1,4.2 5,7.2 9.3,7.1l0.4,0c5.3,0 9.7,-4.3 9.7,-9.7c0,-0.8 -0.1,-1.5 -0.3,-2.2l-16.1,-67.7l-0.3,-0.9a51.19,51.19 0 0 0 -37.9,-32.3c-0.7,-0.1 -1.3,-0.2 -2,-0.2l-28.3,0c-3.5,0 -6.8,-2 -8.4,-5.1c-0.6,-1 -1.2,-2 -1.9,-3c-1.8,-2.4 -4.7,-3.9 -7.7,-3.9l-63.6,0c-2.2,0 -4.4,0.8 -6.1,2.1c-1.8,1.4 -3.3,3.1 -4.5,5c-1.6,2.9 -4.7,4.8 -8.1,4.8l-31.3,0c-1.3,0 -2.6,0.3 -3.8,0.8c-6.6,3 -27.9,13.4 -32.2,29.4c-5.2,14 -12,52.5 -15.2,71.7c-0.9,5.3 2.7,10.3 7.9,11.1c0.7,0.2 1.2,0.2 1.7,0.2z"
         id="svg_2" />`
 }
+function copyAnonymousCartItems(){
+    let tempCartIDs = JSON.parse(sessionStorage.getItem('cartItems'));
+    tempCartIDs.forEach(el => {
+        db.collection('carts').doc(user.uid).update({
+            Products: firebase.firestore.FieldValue.arrayUnion(el)
+        })
+        if(!cartIDs.includes(el))
+            cartIDs.push(el);
+    })
+    
+}
 
 firebase.auth().onAuthStateChanged(_user => {
     if (_user) {
@@ -47,6 +58,16 @@ firebase.auth().onAuthStateChanged(_user => {
             .then(querySnapshot => {
                 cartIDs = querySnapshot.data().Products;
                 if (cartIDs.length > 0) changeCartIcon();
+
+                if(window.sessionStorage.getItem('recentSignIn') === 'true'){
+                    if (JSON.parse(sessionStorage.getItem('cartItems')).length > 0) {
+                        const message = 'There were some items in your cart before signing in.\nDo you wanna add them to your user cart?';
+                        if (confirm(message)) {
+                            copyAnonymousCartItems();
+                        }
+                        sessionStorage.removeItemk('recentSigninIn');
+                    }
+                }
             })
     } else {
         if (confirm('You are not logged in,\nDo you wanna sign in?')) {
